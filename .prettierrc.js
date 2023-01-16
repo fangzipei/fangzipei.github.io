d26460bd8f9b27ceb954f0f21d3660495234d55d
fangzipei <fangzp@glsc.com.cn>
2023-01-16 16:56:11 +0800

    jvm

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    singleQuote: true,
-    useTabs: false,
-    printWidth: 140,
-    tabWidth: 2,
-    semi: true,
-    htmlWhitespaceSensitivity: 'strict',
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-    proseWrap: 'preserve',
-    trailingComma: 'none',
-    endOfLine: 'auto'
-  };
+  singleQuote: true,
+  useTabs: false,
+  printWidth: 130,
+  tabWidth: 2,
+  semi: true,
+  htmlWhitespaceSensitivity: 'strict',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  trailingComma: 'none',
+  endOfLine: 'auto'
+};
